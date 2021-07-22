cbabcd499bc25bb93ca61bd9374a89e13f0eba64
tle1302 <lengochaitrang@gmail.com>
2021-07-22 14:43:24 +0200

    update formatting groundtruth correctly

--- models/research/formatting_gt.py
+++ models/research/formatting_gt.py
@@ -1,8 +1,46 @@
+from cProfile import label
 import pandas as pd
 from object_detection.metrics import oid_challenge_evaluation_utils as utils
+import tqdm
+import imageio
+import os
+import base64
+import numpy as np
+from pycocotools import _mask as coco_mask
+import typing as t
+import zlib
 
-if True:
-    if True:  # FOrmatting the solution files, only need to be done once!
+
+def encode_binary_mask(mask: np.ndarray) -> t.Text:
+  """Converts a binary mask into OID challenge encoding ascii text."""
+
+  # check input mask --
+  if mask.dtype != np.bool:
+    raise ValueError(
+        "encode_binary_mask expects a binary mask, received dtype == %s" %
+        mask.dtype)
+
+  mask = np.squeeze(mask)
+  if len(mask.shape) != 2:
+    raise ValueError(
+        "encode_binary_mask expects a 2d mask, received shape == %s" %
+        mask.shape)
+
+  # convert input mask to expected COCO API input --
+  mask_to_encode = mask.reshape(mask.shape[0], mask.shape[1], 1)
+  mask_to_encode = mask_to_encode.astype(np.uint8)
+  mask_to_encode = np.asfortranarray(mask_to_encode)
+
+  # RLE encode mask --
+  encoded_mask = coco_mask.encode(mask_to_encode)[0]["counts"]
+
+  # compress and base64 encoding --
+  binary_str = zlib.compress(encoded_mask, zlib.Z_BEST_COMPRESSION)
+  base64_str = base64.b64encode(binary_str)
+  return base64_str
+
+if False:
+    if True:  # Formatting the solution files, only need to be done once!
         all_annotations_hpa = pd.read_csv(
             "/home/trangle/HPA_SingleCellClassification/GT/_solution.csv_"
         )
@@ -37,3 +75,31 @@ if True:
             "/home/trangle/HPA_SingleCellClassification/all_annotations.csv",
             index=False,
         )
+
+
+f = open("/home/trangle/HPA_SingleCellClassification/GT/all_annotations_raw.csv", "a+")
+f.write("ImageID,ImageWidth,ImageHeight,ConfidenceImageLabel,LabelName,XMin,YMin,XMax,YMax,IsGroupOf,Mask\n")
+mask_dir = "/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/data_for_Kaggle/data"
+labels = pd.read_csv("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/data_for_Kaggle/labels.csv")
+labels["ImageID"] = [id.split("_")[0] for id in labels.ID]
+labels["cell_idx"] = [id.split("_")[1] for id in labels.ID]
+
+imlist = list(set(labels.ImageID))
+for _, img_id in tqdm.tqdm(enumerate(imlist), total=len(imlist)):
+    image_df = labels[labels.ImageID == img_id]
+    mask = imageio.imread(os.path.join(mask_dir, img_id+"_mask.png"))
+    width = mask.shape[0]
+    height = mask.shape[1]
+    for i, row in image_df.iterrows():
+        cell_idx = row.cell_idx
+        m = mask==int(cell_idx)
+        if m.max() == False:
+            print(f'idx in mask {int(cell_idx)+1}',row)
+        rle = encode_binary_mask(m)
+        boxes = utils._get_bbox(rle, width, height)
+        labs = row.Label.split("|")
+        for l in labs:
+            line = f"{img_id},{width},{height},1,{l},{boxes[1]},{boxes[0]},{boxes[3]},{boxes[2]},0,{rle}\n"
+            f.write(line)
+            #print(line)
+f.close()
