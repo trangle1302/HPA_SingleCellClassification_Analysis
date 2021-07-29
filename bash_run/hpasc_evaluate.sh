83ab7fb1bd31eb10d45a80409abaf2a5b382434a
tle1302 <lengochaitrang@gmail.com>
2021-07-29 17:30:20 +0200

    bash run for test

--- bash_run/hpasc_evaluate.sh
+++ bash_run/hpasc_evaluate.sh
@@ -1,9 +1,9 @@
-ALL_GT_ANNOTATIONS=/home/trangle/HPA_SingleCellClassification/GT/all_annotations.csv
+ALL_GT_ANNOTATIONS=/home/trangle/HPA_SingleCellClassification/GT/all_annotations_sample10.csv
 LABEL_MAP=/home/trangle/HPA_SingleCellClassification/GT/hpa_single_cell_label_map.pbtxt
 INPUT_PREDICTIONS=/home/trangle/HPA_SingleCellClassification/predictions/bestfitting/bestfitting_submission.csv
 OUTPUT_METRICS=/home/trangle/HPA_SingleCellClassification/predictions/bestfitting/bestfitting_outputmetrics.csv
 
-python models/research/object_detection/metrics/oid_hpasc_challenge_evaluation.py \
+python models/research/object_detection/metrics/oid_hpasc_challenge_evaluation_test.py \
     --all_annotations=${ALL_GT_ANNOTATIONS} \
     --input_class_labelmap=${LABEL_MAP} \
     --input_predictions=${INPUT_PREDICTIONS} \
