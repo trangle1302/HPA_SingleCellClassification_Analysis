42c7f6c11c27d047ddc19f8c0d1a6b180b875caf
tle1302 <lengochaitrang@gmail.com>
2021-07-29 17:14:28 +0200

    make make_bash_run.py functional

--- make_bash_run.py
+++ make_bash_run.py
@@ -1,19 +1,31 @@
 import os
 import argparse
+import re
 
-parser = argparse.ArgumentParser(description='Creating bash script')
-parser.add_argument('-folder', type=str, nargs='+', help='path to submission folder')
+parser = argparse.ArgumentParser(description="Creating bash script")
+parser.add_argument("-folder", type=str, help="path to submission folder")
 
 args = parser.parse_args()
 d = "/home/trangle/HPA_SingleCellClassification/"
-f = open(FLAGS.input_predictions.replace(".csv","_formatted.csv"), "a+")
-f.write(f"ALL_GT_ANNOTATIONS={d}/GT/all_annotations_privatetest.csv\n")
-f.write(f"LABEL_MAP={d}/GT/all_annotations_privatetest.csv\n")
-f.write(f"INPUT_PREDICTIONS={d}/{args.folder}\n")
-f.write(f"OUTPUT_METRICS={d}/{args.folder}\n")
 
-f.write("python models/research/object_detection/metrics/oid_hpasc_challenge_evaluation.py \ \n")
-f.write("--all_annotations=${ALL_GT_ANNOTATIONS} \ \n")
-    --input_class_labelmap=${LABEL_MAP} \
-    --input_predictions=${INPUT_PREDICTIONS} \
-    --output_metrics=${OUTPUT_METRICS} \
+file = os.listdir(args.folder)[0]
+name = args.folder.split("/")[-2]
+name = re.search(r"\d+", name)[0]
+
+ALL_GT_ANNOTATIONS = f"{d}/GT/all_annotations_nominus1_nodup.csv"
+LABEL_MAP = f"{d}/GT/hpa_single_cell_label_map.pbtxt"
+INPUT_PREDICTIONS = os.path.join(d, args.folder, file)
+OUTPUT_METRICS = os.path.join(
+    d, args.folder, file.replace(".csv", "_outputmetrics.csv")
+)
+
+# Writing to bash file
+bash_file = f"{d}/bash_run/hpasc_evaluate_{name}.sh"
+if os.path.exists(bash_file):
+    print("found an existing bash file, removing...")
+    os.remove(bash_file)
+f = open(bash_file, "a+")
+f.write(
+    f"python models/research/object_detection/metrics/oid_hpasc_challenge_evaluation.py --all_annotations={ALL_GT_ANNOTATIONS} --input_class_labelmap={LABEL_MAP} --input_predictions={INPUT_PREDICTIONS} --output_metrics={OUTPUT_METRICS} \n"
+)
+f.close()
