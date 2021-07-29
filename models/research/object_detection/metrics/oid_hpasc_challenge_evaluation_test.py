14cec0fc28e6e738b0893cdc9487077ca31af9ec
tle1302 <lengochaitrang@gmail.com>
2021-07-29 17:30:40 +0200

    update testing code

--- models/research/object_detection/metrics/oid_hpasc_challenge_evaluation_test.py
+++ models/research/object_detection/metrics/oid_hpasc_challenge_evaluation_test.py
@@ -53,6 +53,10 @@ from object_detection.protos import string_int_label_map_pb2
 from object_detection.utils import object_detection_evaluation
 import time
 import tqdm
+import os
+import cProfile, pstats, io
+from pstats import SortKey
+from multiprocessing import Process, Lock
 
 flags.DEFINE_string('all_annotations', None,
                     'File with groundtruth boxes and label annotations.')
@@ -68,10 +72,7 @@ flags.DEFINE_string(
     'File with groundtruth instance segmentation annotations [OPTIONAL].')
 
 FLAGS = flags.FLAGS
-FLAGS.all_annotations="/home/trangle/HPA_SingleCellClassification/GT/all_annotations.csv"
-FLAGS.input_class_labelmap="/home/trangle/HPA_SingleCellClassification/GT/hpa_single_cell_label_map.pbtxt"
-FLAGS.input_predictions="/home/trangle/HPA_SingleCellClassification/predictions/bestfitting/en_m11_4image_7cell_0509_complete590.csv"
-FLAGS.output_metrics="/home/trangle/HPA_SingleCellClassification/predictions/bestfitting/en_m11_4image_7cell_0509_complete590_output_metrics.csv"
+
 
 def _load_labelmap(labelmap_path):
   """Loads labelmap from the labelmap path.
@@ -124,14 +125,19 @@ def main(unused_argv):
   
   if False: #Formatting the solution files, only need to be done once!!
       all_annotations_hpa = pd.read_csv("/home/trangle/HPA_SingleCellClassification/GT/_solution.csv_")
+      f = open(FLAGS.all_annotations, "a+")
+      f.write("ImageID,ImageWidth,ImageHeight,ConfidenceImageLabel,LabelName,XMin,YMin,XMax,YMax,IsGroupOf,Mask\n")
       # Testing with 10 images
       #imlist = list(set(all_annotations_hpa.ID))[:10]
       #all_annotations_hpa = all_annotations_hpa[all_annotations_hpa.ID.isin(imlist)]
-      all_annotations = pd.DataFrame()
       for i, row in all_annotations_hpa.iterrows():
           pred_string = row.PredictionString.split(" ")
           for k in range(0, len(pred_string), 7):
               boxes = utils._get_bbox(pred_string[k+6], row.ImageWidth,row.ImageWidth) # ymin, xmin, ymax, xmax
+              if pred_string[k+6] == '-1':
+                continue
+              line = f"{row.ID},{row.ImageWidth},{row.ImageHeight},1,{pred_string[k]},{boxes[1]},{boxes[0]},{boxes[3]},{boxes[2]},{pred_string[k+5]},{pred_string[k+6]}\n"
+              '''
               line = {
                       "ImageID":row.ID,
                       "ImageWidth":row.ImageWidth,
@@ -145,77 +151,76 @@ def main(unused_argv):
                       "IsGroupOf":pred_string[k+5],
                       "Mask": pred_string[k+6]
                 }
-              """
-              binary_mask = decodeToBinaryMask(pred_string[k+6], row.ImageWidth, row.ImageHeight)
-              plt.figure()
-              plt.imshow(binary_mask)
-              """
-              all_annotations = all_annotations.append(line, ignore_index=True)
-      all_annotations_hpa.to_csv("/home/trangle/HPA_SingleCellClassification/all_annotations.csv", index=False)
-  all_annotations = pd.read_csv(FLAGS.all_annotations)
-  all_annotations['ImageHeight'] = all_annotations['ImageHeight'].astype(int)
+              '''
+              print(line)
+              f.write(line)
+      f.close()
+      
+  all_annotations = pd.read_csv(FLAGS.all_annotations, header=0)
+  all_annotations['ImageHeight'] = all_annotations['ImageHeight'].astype(str).astype(int)
   all_annotations['ImageWidth'] = all_annotations['ImageWidth'].astype(int)
-  
+  # Testing with last 3 images
+  imlist = list(set(all_annotations.ImageID))[-3:]
+  all_annotations = all_annotations[all_annotations.ImageID.isin(imlist)]
+
   class_label_map, categories = _load_labelmap(FLAGS.input_class_labelmap)
+  #class_label_map, categories = _load_labelmap("/home/trangle/HPA_SingleCellClassification/models/research/object_detection/data/oid_object_detection_challenge_500_label_map.pbtxt")
   challenge_evaluator = (
       object_detection_evaluation.OpenImagesChallengeEvaluator(
           categories, evaluate_masks=is_instance_segmentation_eval, matching_iou_threshold=0.6))
  
-  submissions = pd.read_csv(FLAGS.input_predictions)      
-  # Testing with 10 images
-  #submissions = submissions[submissions.ID.isin(all_annotations.ImageID)]
-  print(f"{FLAGS.input_predictions} prediction loaded")
-  all_predictions = pd.DataFrame()
-  f = open(FLAGS.input_predictions.replace(".csv","_formatted.csv"), "a+")
-  f.write("ImageID,ImageWidth,ImageHeight,LabelName,Score,Mask\n")
-  for i, row in tqdm.tqdm(submissions.iterrows(),total=submissions.shape[0]):
-      try:
-          pred_string = row.PredictionString.split(" ")
-          for k in range(0, len(pred_string), 3):
-                label = pred_string[k]
-                conf = pred_string[k + 1]
-                rle = pred_string[k + 2]
-                line = f"{row.ID},{row.ImageWidth},{row.ImageHeight},{str(label)},{conf},{rle}\n"
-                f.write(line)
-                """
-                line = {
-                        "ImageID":row.ID,
-                        "ImageWidth":row.ImageWidth,
-                        "ImageHeight":row.ImageHeight,
-                        "LabelName":str(label),
-                        "Score":conf,
-                        "Mask":rle,
-                }
-                all_predictions = all_predictions.append(line, ignore_index=True)
-                """
-      except:
-          continue
+  if not os.path.exists(FLAGS.input_predictions.replace(".csv","_formatted.csv")):
+    submissions = pd.read_csv(FLAGS.input_predictions)      
+    f = open(FLAGS.input_predictions.replace(".csv","_formatted.csv"), "a+")
+    f.write("ImageID,ImageWidth,ImageHeight,LabelName,Score,Mask\n")
+    for i, row in tqdm.tqdm(submissions.iterrows(),total=submissions.shape[0]):
+        try:
+            pred_string = row.PredictionString.split(" ")
+            for k in range(0, len(pred_string), 3):
+                  label = pred_string[k]
+                  conf = pred_string[k + 1]
+                  rle = pred_string[k + 2]
+                  line = f"{row.ID},{row.ImageWidth},{row.ImageHeight},{str(label)},{conf},{rle}\n"
+                  f.write(line)
+        except:
+            continue
 
-  
-  #all_predictions.to_csv(FLAGS.input_predictions.replace(".csv","_formatted.csv"), index=False)
-  
-  """
+  #pr = cProfile.Profile()
+  #pr.enable()
+  all_predictions= pd.read_csv(FLAGS.input_predictions.replace(".csv","_formatted.csv"))
+  all_predictions['LabelName'] = [str(l) for l in all_predictions.LabelName]
   images_processed = 0
-  for _, groundtruth in enumerate(all_annotations.groupby('ImageID')):
-    logging.info('Processing image %d', images_processed)
+  for _, groundtruth in tqdm.tqdm(enumerate(all_annotations.groupby('ImageID')), total=all_annotations.ImageID.nunique()):
+    #if images_processed == 20:
+    #  pass
+    #logging.info('Processing image %d', images_processed)
     image_id, image_groundtruth = groundtruth
     groundtruth_dictionary = utils.build_groundtruth_dictionary(
         image_groundtruth, class_label_map)
     challenge_evaluator.add_single_ground_truth_image_info(
         image_id, groundtruth_dictionary)
+    print(groundtruth_dictionary)
 
     prediction_dictionary = utils.build_predictions_dictionary(
         all_predictions.loc[all_predictions['ImageID'] == image_id],
         class_label_map)
+        
+    print(prediction_dictionary)
     challenge_evaluator.add_single_detected_image_info(image_id,
                                                        prediction_dictionary)
     images_processed += 1
-
   metrics = challenge_evaluator.evaluate()
 
   with open(FLAGS.output_metrics, 'w') as fid:
     io_utils.write_csv(fid, metrics)
-  """
+
+  #pr.disable()
+  #s = io.StringIO()
+  #sortby = SortKey.CUMULATIVE
+  #ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+  #ps.print_stats()
+  #print(s.getvalue())
+
   print(f'Finished in {(time.time() - s)/3600} hour')
 
 if __name__ == '__main__':
