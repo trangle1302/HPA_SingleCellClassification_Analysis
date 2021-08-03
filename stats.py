2391f8313bff0f94fe21d6c5ff272d5e4fb872c4
tle1302 <lengochaitrang@gmail.com>
2021-08-03 15:27:11 +0200

    find all outputmetrics.csv files and merge

--- stats.py
+++ stats.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 d = "/home/trangle/HPA_SingleCellClassification/predictions"
-df = pd.DataFrame()
+df = None
 teams = os.listdir(d)
-teams = [t for t in teams if t!="OID" and os.path.isdir(os.path.join(d,t))]
+teams = [t for t in teams if t != "OID" and os.path.isdir(os.path.join(d, t))]
+
 for t in teams:
-    files = os.listdir(os.path.join(d,t))
+    files = os.listdir(os.path.join(d, t))
     output_f = [f for f in files if f.endswith("_outputmetrics.csv")]
-    if len(output_f)==0:
+    if len(output_f) == 0:
         print(f"Team {t} has no mAP file!")
     else:
-        df_ = pd.read_csv(os.path.join(d,t,output_f[0]), header=None)
-        df_.columns = ["Metrics",t]
-        df = pd.concat([df.reset_index(drop=True), df_], axis=1)
-        print(df)
-
+        df_ = pd.read_csv(os.path.join(d, t, output_f[0]), header=None)
+        df_.columns = ["Metrics", t]
+        if df is None:
+            df = df_
+        else:
+            df = df.merge(df_, on="Metrics")
 df.to_csv(os.path.join(d, "all_outputmetrics.csv"))
