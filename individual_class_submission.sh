893b94e31bc33b44d4ccd51d972b847d3e5db225
tle1302 <lengochaitrang@gmail.com>
2021-08-09 20:50:05 +0200

    how to run

---
+++ individual_class_submission.sh
@@ -0,0 +1,5 @@
+python make_individual_class_submission.py -file=/home/trangle/HPA_SingleCellClassification/predictions/bestfitting/en_m6_2image_4cell_0508_complet_v14.csv
+python make_individual_class_submission.py -file=/home/trangle/HPA_SingleCellClassification/predictions/redai/hpa_final_submission_#2_candidate_II.csv
+python make_individual_class_submission.py -file=/data/kaggle-dataset/mAPscoring/3rd/HPA_Ensemble_v4.9.csv
+python make_individual_class_submission.py -file=/data/kaggle-dataset/mAPscoring/4th/HPASubmission_210272.csv
+
