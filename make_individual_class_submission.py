4bdbdf8f9c253b379c72afcb2363e72ebdd29300
tle1302 <lengochaitrang@gmail.com>
2021-08-09 20:20:39 +0200

    change name

--- make_individual_class_submission.py
+++ make_individual_class_submission.py
@@ -26,6 +26,7 @@ args = parser.parse_args()
 
 
 d = os.path.dirname(args.file)
+name = os.path.basename(d)
 submissions = pd.read_csv(args.file)
 classes = np.arange(0, 19)
 
@@ -33,15 +34,15 @@ for c in classes:
     if os.path.exists(args.file.replace(".csv", f"_class_{c}.csv")):
         print("removing existing class submission file....")
         os.remove(args.file.replace(".csv", f"_class_{c}.csv"))
-    f = open(args.file.replace(".csv", f"_class_{c}.csv"), "a+")
+    f = open(os.path.join(d, f"{name}_class_{c}.csv"), "a+")
+    #f = open(args.file.replace(".csv", f"_class_{c}.csv"), "a+")
     f.write("ID,ImageWidth,ImageHeight,PredictionString\n")
-    print(f"{submissions.shape[0]} images predicted, formatting for class {c}...")
+    print(f"formatting submission for class {c} to {name}_class_{c}.csv")
     for i, row in tqdm.tqdm(submissions.iterrows(), total=submissions.shape[0]):
         try:
             pred_str = filter_pred_str(row.PredictionString, c)
             line = f"{row.ID},{row.ImageWidth},{row.ImageHeight},{pred_str}\n"
             f.write(line)
-
         except Exception as e:
             e = sys.exc_info()
             print("Error Return Type: ", type(e))
