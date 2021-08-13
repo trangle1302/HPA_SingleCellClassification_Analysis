a31c6a0bce96a8e4bcaa39574f9b4fb059dd67bd
tle1302 <lengochaitrang@gmail.com>
2021-08-13 10:28:54 +0200

    add run segmentation

---
+++ run_segmentation_matching.sh
@@ -0,0 +1,31 @@
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/3rd/HPA_Ensemble_v4.9.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/bestfitting/en_m6_2image_4cell_0508_complet_v14.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/redai/hpa_final_submission_#2_candidate_II.csv
+
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/4th/HPASubmission_210272.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/5th/final_submission_narsil_10052021.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/6th/HPA_full_inference_faster_segmentation-v1.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/7th/hpa-final-sub-weight111-f05.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/8th/notebook87aa51323f.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/9th/HPA_Infer_v8.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/10th/p_all_1stx8_2ndx16_Imgx16.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/11th/ForkofCVCombination_EfficientNet-B3.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/12th/TratzHPA-Accelerated.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/13th/Ensemble_DiverseDMv1_segmentationBCPDM.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/14th/HPA_Submission_Model_Ensemble.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/15th/hpa-cam-infer.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/16th/hpa_yolo5_ensemble_final.csv
+
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/18th/HPA_kaggle_GPU.cs
+
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/20th/HPA_TTA_Cell_20894230.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/21st/HPA_Inferencing_20994237.csv
+
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/23rd/densenet_cellwis_seg_from_scratch.csv 
+
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/25th/hpa-single-cell_PrivateLB_sub_imglvl_mix.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/26th/INFER_OFFLINE_ADJUSTABLE_ENSELBLE_ic_focal_mix.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/27th/HPA_submission_20992911.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/28th/final-4.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/29th/HPA-fastai-submission_21020739.csv
+python segmentation_matching.py -file=/data/kaggle-dataset/mAPscoring/30th/hpa-single-cell-enselble_20987471.csv
