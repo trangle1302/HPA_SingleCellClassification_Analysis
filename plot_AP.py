d54108e405dbce3b12cefabb96edbec166fa1598
tle1302 <lengochaitrang@gmail.com>
2021-09-28 17:10:01 +0200

    update GT_cell_count and save jpg

--- plot_AP.py
+++ plot_AP.py
@@ -3,6 +3,7 @@ import io
 import json
 #!pip install pandas plotnine
 from plotnine import *
+import os
 
 def read_from_json(json_file_path):
     """Function to read json file (annotation file)
@@ -28,33 +29,56 @@ def get_location_counts(label_list, all_locations):
                 label_counts[loc] += 1
     return label_counts
     
-
-df = pd.read_excel("/home/trangle/HPA_SingleCellClassification/Kaggle_AP.xlsx", sheet_name="Sheet1")
-#mapping = pd.read_csv("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/mappings.csv")
-mapping = read_from_json("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/all_locations.json")
-mapping.update({'Negative': 18})
+save_dir = "/home/trangle/HPA_SingleCellClassification"
+df = pd.read_excel(os.path.join(save_dir, "Kaggle_AP.xlsx"), sheet_name="Sheet1")
+mapping = dict({
+    "Nucleoplasm": 0,
+    "Nuc.Membrane": 1,
+    "Nucleoli": 2,
+    "Nuc.Fib.C": 3,
+    "Nuc.Speckles": 4,
+    "Nuc.Bodies": 5,
+    "ER": 6,
+    "Golgi": 7,
+    "Int.Fil": 8,
+    "Actin.Fil": 9,
+    "Microtubules": 10,
+    "M.Spindle": 11,
+    "Centrosome": 12,
+    "Pl.Membrane": 13,
+    "Mitochondria": 14,
+    "Aggresome": 15,
+    "Cytosol": 16,
+    "Ves.Punctate": 17,
+    "Negative" : 18
+})
 reverse_mapping = {str(v):k for k,v in mapping.items()}
-df["LabelName"] = [reverse_mapping[str(l)] for l in df.Class]
+df["ShortLabelName"] = [reverse_mapping[str(l)] for l in df.Class]
 
 
 ### AP plot
 # Plotting order = mean of each class
-order = df.groupby(["LabelName"]).mean().AP.sort_values(ascending=True).index
-df.LabelName = pd.Categorical(df.LabelName, 
+order = df.groupby(["ShortLabelName"]).mean().AP.sort_values(ascending=True).index
+df.ShortLabelName = pd.Categorical(df.ShortLabelName, 
                              ordered=True,
                              categories=order)
-order = df.groupby(["LabelName"]).mean().AP.sort_values(ascending=True).index
-g = ggplot(df, aes(x="LabelName", y="AP",fill="LabelName"))
+order = df.groupby(["ShortLabelName"]).mean().AP.sort_values(ascending=True).index
+g = ggplot(df, aes(x="ShortLabelName", y="AP",fill="LabelName"))
 g = g + geom_violin() + theme(axis_text_x = element_text(angle=90, hjust=1)) + geom_point()
-
+g.save(os.path.join(save_dir, "plots","AP_per_class.jpg"), dpi=600)
 
 
 ### AP vs sample counts
-gt_labels = pd.read_csv("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/data_for_Kaggle/publictest/labels_publictest.csv")
+gt_labels_pub = pd.read_csv("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/data_for_Kaggle/publictest/labels_publictest.csv")
+gt_labels_pri = pd.read_csv("/home/trangle/Desktop/annotation-tool/HPA-Challenge-2020-all/data_for_Kaggle/privatetest/labels_privatetest.csv")
+gt_labels = pd.concat([gt_labels_pub, gt_labels_pri])
 gt_labels["Image_ID"] = [f.split("_")[0] for f in gt_labels.ID]
 gt_labels["Cell_ID"] = [f.split("_")[1] for f in gt_labels.ID]
 label_counts = get_location_counts(list(gt_labels.Label), mapping)
-df_ = df.groupby(["LabelName"]).mean()
-df_["LabelName"] = df_.index
-df_["GT_count"] = [label_counts[l] for l in df_.index]
-ggplot(df_, aes(x="LabelName", y ="GT_count", fill="LabelName")) + geom_bar(stat = 'identity')  + theme(axis_text_x = element_text(angle=90, hjust=1))
+df_ = df.groupby(["ShortLabelName"]).mean()
+df_["ShortLabelName"] = df_.index
+df_["GT_cell_count"] = [label_counts[l] for l in df_.index]
+ggplot(df_, aes(x="ShortLabelName", y ="GT_cell_count", fill="ShortLabelName")) + geom_bar(stat = 'identity')  + theme(axis_text_x = element_text(angle=90, hjust=1))
+
+p = ggplot(df_, aes(x="AP", y ="GT_cell_count", fill="ShortLabelName")) + geom_point() + xlim(0.2, 0.8)  + ylim(0, 12000) + geom_text(aes(label="ShortLabelName"), size=7, angle=20, ha='left', va='bottom')
+p.save(os.path.join(save_dir, "plots","Cellcounts_vs_AP.jpg"), dpi=600)
