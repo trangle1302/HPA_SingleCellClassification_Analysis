e97a1be6e5861f05d665e272eaa3cf36163f849d
tle1302 <lengochaitrang@gmail.com>
2021-11-25 21:03:21 +0100

    update plots, change color

--- plot_AP.py
+++ plot_AP.py
@@ -55,6 +55,15 @@ mapping = dict({
 reverse_mapping = {str(v):k for k,v in mapping.items()}
 df["ShortLabelName"] = [reverse_mapping[str(l)] for l in df.Class]
 
+COLORS = [
+    '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+    '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50',
+    '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800',
+    '#ff5722', '#795548', '#9e9e9e', '#607d8b', '#dddddd',
+    '#212121', '#ff9e80', '#ff6d00', '#ffff00', '#76ff03',
+    '#00e676', '#64ffda', '#18ffff',
+]
+
 
 ### AP plot
 # Plotting order = mean of each class
@@ -62,10 +71,10 @@ order = df.groupby(["ShortLabelName"]).mean().AP.sort_values(ascending=True).ind
 df.ShortLabelName = pd.Categorical(df.ShortLabelName, 
                              ordered=True,
                              categories=order)
-order = df.groupby(["ShortLabelName"]).mean().AP.sort_values(ascending=True).index
-g = ggplot(df, aes(x="ShortLabelName", y="AP",fill="LabelName"))
-g = g + geom_violin() + theme(axis_text_x = element_text(angle=90, hjust=1)) + geom_point()
-g.save(os.path.join(save_dir, "plots","AP_per_class.jpg"), dpi=600)
+colorvector = [COLORS[mapping[n]] for n in order]
+g = ggplot(df, aes(x="ShortLabelName", y="AP", fill="ShortLabelName"))
+g = g + geom_violin() + theme_classic() + theme(axis_text_x = element_text(angle=90, hjust=1)) + geom_point()+ scale_fill_manual(values=list(colorvector))
+g.save(os.path.join(save_dir, "plots","AP_per_class2.jpg"), dpi=600)
 
 
 ### AP vs sample counts
@@ -80,5 +89,5 @@ df_["ShortLabelName"] = df_.index
 df_["GT_cell_count"] = [label_counts[l] for l in df_.index]
 ggplot(df_, aes(x="ShortLabelName", y ="GT_cell_count", fill="ShortLabelName")) + geom_bar(stat = 'identity')  + theme(axis_text_x = element_text(angle=90, hjust=1))
 
-p = ggplot(df_, aes(x="AP", y ="GT_cell_count", fill="ShortLabelName")) + geom_point() + xlim(0.2, 0.8)  + ylim(0, 12000) + geom_text(aes(label="ShortLabelName"), size=7, angle=20, ha='left', va='bottom')
+p = ggplot(df_, aes(x="AP", y ="GT_cell_count", fill="ShortLabelName")) + geom_point() + theme_classic() + xlim(0.2, 0.8) + ylim(0, 12000) + geom_text(aes(label="ShortLabelName"), size=7, angle=20, ha='left', va='bottom') 
 p.save(os.path.join(save_dir, "plots","Cellcounts_vs_AP.jpg"), dpi=600)
