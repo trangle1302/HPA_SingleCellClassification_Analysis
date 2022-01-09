5928c1b9b9c1139e86766b54a2caa96d805e7839
tle1302 <lengochaitrang@gmail.com>
2022-01-09 20:11:00 +0100

    update for 3rd team

--- generate_cam_single_cell.py
+++ generate_cam_single_cell.py
@@ -1,46 +1,56 @@
 import glob
+import os
+import cv2
 import imageio
 from skimage.measure import regionprops
 
 MASK_DIR="/data/kaggle-dataset/CAM_images/mask/train"
-IMAGE_DIR="/data/kaggle-dataset/CAM_images/2nd_cams"
-SAVE_DIR="/data/kaggle-dataset/CAM_images/2nd_cams"
+IMAGE_DIR="/data/kaggle-dataset/CAM_images/3rd_cams/cams"
+SAVE_DIR="/data/kaggle-dataset/CAM_images/3rd_cams"
 
 LABEL_TO_ALIAS = {
-  0: 'Nucleoplasm',
-  1: 'NuclearM',
-  2: 'Nucleoli',
-  3: 'NucleoliFC',
-  4: 'NuclearS',
-  5: 'NuclearB',
-  6: 'EndoplasmicR',
-  7: 'GolgiA',
-  8: 'IntermediateF',
-  9: 'ActinF',
-  10: 'Microtubules',
-  11: 'MitoticS',
-  12: 'Centrosome',
-  13: 'PlasmaM',
-  14: 'Mitochondria',
-  15: 'Aggresome',
-  16: 'Cytosol',
-  17: 'VesiclesPCP',
-  18: 'Negative',
+    0: 'Nucleoplasm',
+    1: 'NuclearM',
+    2: 'Nucleoli',
+    3: 'NucleoliFC',
+    4: 'NuclearS',
+    5: 'NuclearB',
+    6: 'EndoplasmicR',
+    7: 'GolgiA',
+    8: 'IntermediateF',
+    9: 'ActinF',
+    10: 'Microtubules',
+    11: 'MitoticS',
+    12: 'Centrosome',
+    13: 'PlasmaM',
+    14: 'Mitochondria',
+    15: 'Aggresome',
+    16: 'Cytosol',
+    17: 'VesiclesPCP',
+    18: 'Negative',
 }
 
 def main():
-    imlist = glob.glob(f"{IMAGE_DIR}/")
     masklist = glob.glob(f"{MASK_DIR}/*_cellmask.png")
-    print(f"Found {len(masklist)} masks")
+    print(f"Found {len(masklist)} masks, example path {masklist[0]}")
     for mask_path in masklist:
+        img_name = os.path.basename(mask_path).replace("_cellmask.png","")
         mask = imageio.imread(mask_path)
-        img = imageio.imread(glob.glob(f"{IMAGE_DIR}/{img_name}_*.png"))
-        regions = regionprops(mask)
-        for region in regions:
-            bbox = region.bbox
-            cell_id = region.label
-            cell = img[bbox[0]:bbox[1],bbox[2]:bbox[3],:]
-            imageio.imwrite(f"{SAVE_DIR}/{img_name}_{cell_id}_{location}.png", cell)
+        img_paths = glob.glob(f"{IMAGE_DIR}/{img_name}_*.png")
+        for img_path in img_paths:
+            #print(f"{IMAGE_DIR}/{img_name}_*.png",img_path)
+            location = img_path.split("_")[-1].replace(".png", "")
+            img = imageio.imread(img_path)
+            mask = cv2.resize(mask, (512,512))
+            #print(f"Mask shape: {mask.shape}, Image shape: {img.shape}")
+            regions = regionprops(mask)
+            for region in regions:
+                bbox = region.bbox
+                #print(bbox, mask.shape, img.shape)
+                cell_id = region.label
+                cell = img[bbox[0]:bbox[2],bbox[1]:bbox[3],:]
+                cell = cv2.resize(cell, (128,128))
+                imageio.imwrite(f"{SAVE_DIR}/{img_name}_{cell_id}_{location}.png", cell)
 
 if __name__ == '__main__':
     main()
